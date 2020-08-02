496c788b7a7eb55a0c6166f36a163e4d113756ea
kaankaray <kaankaray99@gmail.com>
2020-08-02 19:47:47 +0300

    Initial Commit

---
+++ Gossp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gossp
+//
+//  Created by Kaan Karay on 2.08.2020.
+//  Copyright © 2020 Kaan Karay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
