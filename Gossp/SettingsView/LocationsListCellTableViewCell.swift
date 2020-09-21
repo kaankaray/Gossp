4477f69b0ad4fce8d4f22c76d76e215f1e9c4b74
kaankaray <kaankaray99@gmail.com>
2020-09-21 15:19:17 +0300

    Rewritten LocationsListCellTableViewCell - Better subscription alert. - new useData function which updates the view completely - Added taptic feedback - Rewritten unsubscription fully

--- Gossp/SettingsView/LocationsListCellTableViewCell.swift
+++ Gossp/SettingsView/LocationsListCellTableViewCell.swift
@@ -16,46 +16,46 @@ class LocationsListCellTableViewCell: UITableViewCell {
   @IBOutlet var subscriptionButton: UIButton!
   @IBOutlet var secondLabel: UILabel!
   
-  
-  var gosspLocation:GosspLocation = GosspLocation()
-  var customSuperView:MapViewController = MapViewController()
-  var customSuperViewSocial:SocialViewController = SocialViewController()
-  
-  func useData(_ loc:GosspLocation) {
-    gosspLocation = loc
-    
-      titleLabel.text = gosspLocation.name
-      titleLabel.textColor = gosspLocation.color(alpha: 1)
-      contributersLabel.text = String(gosspLocation.contCount.count)
-      secondLabel.text = String(format: "%.2f", gosspLocation.distance) + " meters away."
-      if currentUser.enlistedLocations.contains(gosspLocation.ID){
-        subscriptionButton.setTitle("Unsubscribe from this Gossp location.", for: .normal)
-        subscriptionButton.tintColor = .systemRed
-      } else {
-        subscriptionButton.setTitle("Subscribe to this Gossp location.", for: .normal)
-        subscriptionButton.tintColor = .gosspGreen
-      }
-      backgroundColor = .gosspPurple
-      contributersLabel.textColor = .gosspGreen
-      secondLabel.textColor = .gosspGreen
-      userIcon.tintColor = .gosspGreen
-    
-    
-  }
-  
   @IBAction func subscriptionButtonAct(_ sender: Any) {
-    if currentUser.enlistedLocations.contains(gosspLocation.ID){
+    
+    if locationList.contains(gosspLocation.ID){
+      randTaptic(1)
+      let k = NSMutableAttributedString()
+        .normal("Please note that once you ")
+        .bold("unsubscribe")
+        .normal(" from a Gossp location, you can only subscribe again ")
+        .bold("only if")
+        .normal(" you are 100 meters near to that Gossp location. And, once you unsubscribe, your ID in your Gossps and in your comments on any Gossps, your name will get strikethough ")
+        .crossed("like this")
+        .normal(".")
       
-      let alert = UIAlertController(title: "Are you sure?", message: "Please be advised that once you unsubscribe from a location, you can only subscribe again if you are near to that Gossp location. Users in the location won't know that you left.", preferredStyle: .actionSheet)
+      
+      let alert = UIAlertController(title: "Are you sure?", message: "", preferredStyle: .actionSheet)
+      alert.setValue(k, forKey: "attributedMessage")
       
       alert.addAction(UIAlertAction(title: "Unsubscribe", style: .destructive, handler: { (action) in
-        currentUser.enlistedLocations.remove(at: currentUser.enlistedLocations.firstIndex(of: self.gosspLocation.ID)!)
-        self.gosspLocation.contCount.remove(at: self.gosspLocation.contCount.firstIndex(of: currentUser.name)!)
-        currentUser.updateCloud()
+        randTaptic(0)
+        
+        self.subscriptionButton.animateBounceTwice()
+        
+        
+        locationList.remove(at: locationList.firstIndex(of: self.gosspLocation.ID)!)
+        for k in 0...self.gosspLocation.contCount.count-1{
+          if self.gosspLocation.contCount[k].pNumber == accPNumber{self.gosspLocation.contCount[k].isDeleted = true}
+        }
+        for k in 0...self.gosspLocation.GosspArray.count-1{
+          // Update creators of this user. As deleted.
+          if self.gosspLocation.GosspArray[k].creator.pNumber == accPNumber {
+            self.gosspLocation.GosspArray[k].creator.isDeleted = true
+            ref.child("locations").child("value\(self.gosspLocation.ID)").child("GosspArray").child("creator").setValue(self.gosspLocation.GosspArray[k].creator.returnAsDictionary())
+          }
+        }
+        
+        updateAccount()
         self.gosspLocation.updateCloud()
         self.useData(self.gosspLocation)
       }))
-      alert.addAction(UIAlertAction(title: "Nevermind", style: .cancel, handler: nil))
+      alert.addAction(UIAlertAction(title: "Nevermind", style: .cancel, handler: {(action) in randTaptic(7)}))
       //if customSuperView.allLocations.count != 0{} Will this check work?
       //customSuperViewSocial.present(alert, animated: true)
       customSuperView.present(alert, animated: true)
@@ -63,17 +63,76 @@ class LocationsListCellTableViewCell: UITableViewCell {
       
     }
     else {
-      currentUser.enlistedLocations.append(gosspLocation.ID)
-      gosspLocation.contCount.append(currentUser.name)
-      currentUser.updateCloud()
-      gosspLocation.updateCloud()
-      useData(gosspLocation)
+      addOrActivateUser()
+      subscriptionButton.animateBounceTwice()
+      randTaptic(4)
     }
+  }
+  
+  var gosspLocation:GosspLocation = GosspLocation()
+  var customSuperView:MapViewController = MapViewController()
+  
+  
+  
+  ///Checks if the user already exists. If exists, reactivates the acc by changing isDeleted to false. If doesn't exist, creates the user.
+  func addOrActivateUser() {
+    locationList.append(gosspLocation.ID)
+    for k in (self.gosspLocation.contCount.count-1) * -1...0{
+      if self.gosspLocation.contCount[-k].pNumber == accPNumber{
+        self.gosspLocation.contCount[-k].isDeleted = false
+        break
+      }
+      if k == 0 && self.gosspLocation.contCount[-k].pNumber != accPNumber {
+        self.gosspLocation.contCount.append(GosspUser.init(withLocation: self.gosspLocation))
+      }
+    }
+    updateAccount()
+    gosspLocation.updateCloud()
+    useData(gosspLocation)
+  }
+  
+  ///Removes deleted accounts from contributer count and returns it.
+  func getRealContributerCount(_ arr:Array<GosspUser>) -> Int{
+    var t = arr.count
+    arr.forEach{if $0.isDeleted{t -= 1}}
+    
+    return t
+  }
+  
+  ///Updates the view completely. Adds all the new data.
+  func useData(_ loc:GosspLocation) {
+    gosspLocation = loc
+    
+    titleLabel.text = gosspLocation.name
+    titleLabel.textColor = gosspLocation.color(alpha: 1)
+    contributersLabel.text = String(getRealContributerCount(gosspLocation.contCount))
+    secondLabel.text = metersToKM(meters: gosspLocation.distance) + " away."
+    if !(gosspLocation.distance < minDistanceToAction) {subscriptionButton.isHidden = true}
+    else {subscriptionButton.isHidden = false}
+    if locationList.contains(gosspLocation.ID){
+      //      subscriptionButton.setTitle("Unsubscribe from this Gossp location.", for: .normal)
+      subscriptionButton.setImage(UIImage(named: "starFilled.png"), for: .normal)
+      subscriptionButton.tintColor = .systemRed
+      subscriptionButton.isHidden = false
+    } else {
+      //      subscriptionButton.setTitle("Subscribe to this Gossp location.", for: .normal)
+      subscriptionButton.setImage(UIImage(named: "star.png"), for: .normal)
+      subscriptionButton.tintColor = .gosspGreen
+    }
+    backgroundColor = .gosspPurple
+    contributersLabel.textColor = .gosspGreen
+    secondLabel.textColor = .white
+    userIcon.tintColor = .gosspGreen
+    
+    
   }
   
   override func awakeFromNib() {
     super.awakeFromNib()
     // Initialization code
+    backgroundColor = .gosspPurple
+    tintColor = .gosspGreen
+    
     
   }
   
